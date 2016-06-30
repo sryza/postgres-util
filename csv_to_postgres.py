1f8c8c52663c1e0c543bf5291f804f0fc696deea
Sandy Ryza <sandyryza@gmail.com>
2016-06-29 17:42:46 -0700

    Lowercase and underscore column names, add header docs, and spread output across multiple lines

--- csv_to_postgres.py
+++ csv_to_postgres.py
@@ -1,4 +1,18 @@
+"""
+Usage:
+    python csv_to_postgres.py <csv file name> <schema_name.table_name>
+"""
+
 import sys
+import re
+
+
+def format_column_name(col_name):
+    col_name = re.sub(r'([a-z])([A-Z])', r'\1_\2', col_name)
+    col_name = col_name.replace(' ', '_')
+    col_name = col_name.lower()
+    return col_name
+
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
@@ -6,9 +20,10 @@ if __name__ == '__main__':
 
     first_line = open(file_name).readline()[:-1]
     column_names = first_line.split(',')
+    column_names = map(format_column_name, column_names)
 
-    column_definitions_str = ', '.join(['\"%s\" varchar' % col for col in column_names])
-    create_table_command = 'CREATE TABLE %s (%s);' % (table_name, column_definitions_str)
+    column_definitions_str = '\n  ' + ',\n  '.join(['\"%s\" varchar' % col for col in column_names])
+    create_table_command = 'CREATE TABLE %s (%s\n);' % (table_name, column_definitions_str)
     copy_file_command = "\\copy %s FROM \'%s\' DELIMITER ',' CSV HEADER;" % (table_name, file_name)
 
     print(create_table_command)
